90e35c3cf4f2394b6f3edffe89ce8e83ced61546
Aman Gupta <guptakunalx@gmail.com>
2023-05-25 18:14:32 +0530

    dropdown-component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+import Dropdown from "./ninja-ui/dropdown";
 import Tooltip from "./ninja-ui/tooltip";
+
 
 // creating a tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 tooltip.init();
+
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach(dropdown => {
+    const dropdownContainer = new Dropdown(dropdown);
+
+    dropdownContainer.init();
+});
