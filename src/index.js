1812d997a2bfaca2be9224f45cf09009767cedf6
Aman Gupta <guptakunalx@gmail.com>
2023-05-25 16:27:33 +0530

    tooltip-component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import "./test.css";
+import Tooltip from "./ninja-ui/tooltip";
 
-console.log(`Hello, World`);
+// creating a tooltip
+const tooltip = new Tooltip(document.querySelector(".tooltip"));
+tooltip.init();
